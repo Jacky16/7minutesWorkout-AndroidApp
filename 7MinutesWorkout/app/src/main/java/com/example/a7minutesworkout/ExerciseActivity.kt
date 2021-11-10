090e86d4f818a9936733357ccd7f4a2797ae451d
Adria Martinez <adriamartinez94@gmail.com>
2021-11-10 12:45:53 +0100

    Hecha la pantalla principal

---
+++ 7MinutesWorkout/app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -0,0 +1,24 @@
+package com.example.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a7minutesworkout.databinding.ActivityExerciseBinding
+import com.example.a7minutesworkout.databinding.ActivityMainBinding
+
+class ExerciseActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityExerciseBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbarExercise)
+
+        if(supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding.toolbarExercise.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+    }
+}
---
+++ 7MinutesWorkout/app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.a7minutesworkout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.example.a7minutesworkout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //val flStartButton: FrameLayout = findViewById(R.id.flStart)
+        binding.flStart.setOnClickListener{
+            val intent = Intent(this,ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+
+}
